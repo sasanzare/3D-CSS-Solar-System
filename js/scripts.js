1f3531195c805e7f4bf652cf36011c28aa237b1b
sasanzare <sasanzare75@gmail.com>
2026-07-12 21:13:28 +0100

    Optimize and lighten the codebase

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,71 @@
-$(window).load(function(){
+window.addEventListener('load', function() {
 
-  var body = $("body"),
-      universe = $("#universe"),
-      solarsys = $("#solar-system");
+  var body = document.body,
+      universe = document.getElementById('universe'),
+      solarsys = document.getElementById('solar-system');
+
+  // Class helpers
+  function addClass(el, cls) { el.classList.add(cls); }
+  function removeClass(el, cls) { el.classList.remove(cls); }
+  function toggleClass(el, cls) { el.classList.toggle(cls); }
 
   var init = function() {
-    body.removeClass('view-2D opening').addClass("view-3D").delay(2000).queue(function() {
-      $(this).removeClass('hide-UI').addClass("set-speed");
-      $(this).dequeue();
-    });
+    removeClass(body, 'view-2D');
+    removeClass(body, 'opening');
+    addClass(body, 'view-3D');
+    setTimeout(function() {
+      removeClass(body, 'hide-UI');
+      addClass(body, 'set-speed');
+    }, 2000);
   };
 
-  var setView = function(view) { universe.removeClass().addClass(view); };
+  var setView = function(view) {
+    universe.className = view;
+  };
 
-  $("#toggle-data").click(function(e) {
-    body.toggleClass("data-open data-close");
+  document.getElementById('toggle-data').addEventListener('click', function(e) {
+    toggleClass(body, 'data-open');
+    toggleClass(body, 'data-close');
     e.preventDefault();
   });
 
-  $("#toggle-controls").click(function(e) {
-    body.toggleClass("controls-open controls-close");
+  document.getElementById('toggle-controls').addEventListener('click', function(e) {
+    toggleClass(body, 'controls-open');
+    toggleClass(body, 'controls-close');
     e.preventDefault();
   });
 
-  $("#data a").click(function(e) {
-    var ref = $(this).attr("class");
-    solarsys.removeClass().addClass(ref);
-    $(this).parent().find('a').removeClass('active');
-    $(this).addClass('active');
-    e.preventDefault();
-  });
+  var dataLinks = document.querySelectorAll('#data a');
+  for (var i = 0; i < dataLinks.length; i++) {
+    dataLinks[i].addEventListener('click', function(e) {
+      var ref = this.className;
+      solarsys.className = ref;
+      var siblings = this.parentNode.querySelectorAll('a');
+      for (var j = 0; j < siblings.length; j++) {
+        removeClass(siblings[j], 'active');
+      }
+      addClass(this, 'active');
+      e.preventDefault();
+    });
+  }
 
-  $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
-  $(".set-zoom").click(function() { body.toggleClass("zoom-large zoom-close"); });
-  $(".set-speed").click(function() { setView("scale-stretched set-speed"); });
-  $(".set-size").click(function() { setView("scale-s set-size"); });
-  $(".set-distance").click(function() { setView("scale-d set-distance"); });
+  document.querySelector('.set-view').addEventListener('click', function() {
+    toggleClass(body, 'view-3D');
+    toggleClass(body, 'view-2D');
+  });
+  document.querySelector('.set-zoom').addEventListener('click', function() {
+    toggleClass(body, 'zoom-large');
+    toggleClass(body, 'zoom-close');
+  });
+  document.querySelector('.set-speed').addEventListener('click', function() {
+    setView('scale-stretched set-speed');
+  });
+  document.querySelector('.set-size').addEventListener('click', function() {
+    setView('scale-s set-size');
+  });
+  document.querySelector('.set-distance').addEventListener('click', function() {
+    setView('scale-d set-distance');
+  });
 
   init();
 
